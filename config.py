456509f4407f51eb306d3c092f05e0618d2532f1
Waseem281 <mdw2804@gmail.com>
2026-06-27 18:57:37 +0530

    Support Streamlit secrets

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+if "GROQ_API_KEY" in st.secrets:
+    GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
+else:
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 MODEL_NAME = "llama-3.1-8b-instant"
